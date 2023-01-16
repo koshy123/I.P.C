3cf0e53f491eb43e0c920f8f68064321e855d6d5
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-15 23:53:54 -0500

    changes made

--- church/core/models.py
+++ church/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profilmg = models. ImageField()
+    location = pass
